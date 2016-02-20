9148b60f565ec83faa40e6efb93daa34cfeefb77
Karan Kumar <karankumar.world@gmail.com>
2016-02-21 02:47:10 +0530

    adding selected class to the hero selcted.

--- heroes/app.components.ts
+++ heroes/app.components.ts
@@ -22,7 +22,7 @@ interface Hero {
       </div>
       <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="#hero of heroes"  (click)="onSelect(hero)" >
+            <li *ngFor="#hero of heroes"  (click)="onSelect(hero)" [class.selected]="hero === selectedHero" >
                  <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
